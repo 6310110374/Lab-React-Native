3b8b7fc631be947676c9d63f13f554ca16f30f9a
6310110374 <6310110374@psu.ac.th>
2022-08-27 16:56:22 +0700

    part 3, CHALLENGE

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,16 +1,53 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
 
 export default function Forecast(props) {
     return (
         <View>
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
             <View>
-                <Text>{props.temp}</Text>
-                <Text>°C</Text>
+                <Text style = {styles.main}>main</Text>
+                <Text style = {styles.description}>{props.main}</Text>
+                <Text style = {styles.description}>description</Text>
+                <Text style = {styles.description}>{props.description}</Text>
+            </View>
+
+            <View style = {{flexDirection: "row", justifyContent: 'center'}}>
+                <Text style = {styles.value}>{props.temp}</Text>
+                <Text style = {styles.temp}>°C</Text>
             </View>
         </View>
     )
 }
-   
+
+const styles = StyleSheet.create({
+    main: {
+        marginTop: Constants.statusBarHeight,
+        fontSize: 40,
+        fontWeight: "bold",
+        textAlign: 'center',
+        color: 'white',
+    },
+
+    description: {
+        fontSize: 25,
+        textAlign: 'center',
+        color: 'white',
+    },
+
+    value: {
+        fontSize: 40, 
+        fontWeight: "norml", 
+        color: 'white', 
+        textAlign: 'center',
+
+    },
+
+    temp: {
+        fontSize: 24,
+        textAlign: 'center',
+        color: 'white',
+        lineHeight: 35,
+    },
+
+});
--- components/Weather.js
+++ components/Weather.js
@@ -1,27 +1,44 @@
 import React, {useState} from 'react';
-import { Text, ImageBackground, StyleSheet} from 'react-native';
+import { Text, ImageBackground, StyleSheet, View} from 'react-native';
 import Forecast from './Forecast';
+import Constants from 'expo-constants';
 
 export default function Weather(props) {
-    const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
+    const [forecastInfo, setForecastInfo] = useState({         
+        main: '',
+        description: '',
         temp: 0
     }) 
 
     return (
-            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <Text>Zip Code</Text>
-                <Text>{props.zipCode}</Text>
-                <Forecast {...forecastInfo} />
+            <ImageBackground source = {require('../bg.jpg')} style={styles.backdrop}>
+                <View style={styles.proportion}>
+                        <Text style = {styles.text}>Zip code is {props.zipCode}.</Text>
+                        <Forecast {...forecastInfo}/>
+                </View>
             </ImageBackground>
     );
-}
+} 
 
 const styles = StyleSheet.create({
     backdrop: {
         alignItems: 'center',
         width: '100%',
         height: '100%'
-    }
+    },
+
+    text: {
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center'
+    },
+
+    proportion: {
+        backgroundColor: 'rgba(0, 0, 0, 0.45)',
+        width:"100%", 
+        height:"40%", 
+        paddingTop: Constants.statusBarHeight, 
+        alignItems: 'center'
+    },
+
 });
